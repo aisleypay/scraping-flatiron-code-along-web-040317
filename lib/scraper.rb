518039abe8501802e36537401d051123e8ebe94f
aisleypay <paisley.singh@gmail.com>
2017-04-10 09:02:56 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
+
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
@@ -15,8 +17,45 @@ class Scraper
       end
     end
   end
-  
+
+# The get_page instance method will be responsible for using Nokogiri and open-uri to grab the entire HTML document from the web page.
+
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+  end
+
+# The #get_courses instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course. In other words, the return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering. We're going to have to examine the page with the element inspector to find the CSS selector that contains the courses.
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  # The #make_courses method will be responsible for actually instantiating Course objects and giving each course object the correct title, schedule and description attribute that we scraped from the page.
+
+  def make_courses
+    #collection of courses
+    # doc.css(".post")
+    # #title
+    # doc.css(".post").first.css("h2").text
+    # #course schedule
+    # doc.css(".post").first.css(".date").text
+    # #course description
+    # doc.css(".post").first.css("p").text
+
+    self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+  end
+
+
 end
-
-
-
